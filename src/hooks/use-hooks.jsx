5ac2fba5d3556f75cd5b997ff2deaf182da3606f
X_Abhi <abhishekvaidya8766@gmail.com>
2026-03-05 14:44:02 +0530

    Delete src directory

--- src/hooks/use-hooks.jsx
+++
@@ -1,6 +0,0 @@
-
-export function useCaptcha() {
-    let code = ''
-    code = `${Math.round(Math.random()*10)} ${Math.round(Math.random()*10)} ${Math.round(Math.random()*10)} ${Math.round(Math.random()*10)} ${Math.round(Math.random()*10)} ${Math.round(Math.random()*10)}`
-    return code
-}
--- src/video-library/user/user-dashboard.jsx
+++
@@ -1,8 +0,0 @@
-
-export function UserDashboard() {
-    return(
-        <div>
-            <h2>User dashboard</h2>
-        </div>
-    )
-}
